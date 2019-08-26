b6e0ae2bb6e358fe4fdb357d591717e94ffb9ea8
mystic-cg <215104920@qq.com>
2019-08-26 20:58:51 +0800

    [learning] drawing

--- data_analysis.py
+++ data_analysis.py
@@ -2,9 +2,8 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
 
+import matplotlib.pyplot as plt
 import networkx as nx
-
-from find_optimal import find_optimal
 
 
 def read_data(file):
@@ -23,7 +22,11 @@ if __name__ == '__main__':
     print("Read Data cost: {}s".format(end - start))
     undirected_graph = nx.Graph()
     undirected_graph.add_edges_from(data)
-    start = datetime.now()
-    print(find_optimal(undirected_graph))
-    end = datetime.now()
-    print("Find optimal cost: {}s".format(end - start))
+
+    nx.draw(undirected_graph, with_labels=True, font_weight='bold', node_color='y', )
+    plt.show()
+
+    # start = datetime.now()
+    # print(find_optimal(undirected_graph))
+    # end = datetime.now()
+    # print("Find optimal cost: {}s".format(end - start))
