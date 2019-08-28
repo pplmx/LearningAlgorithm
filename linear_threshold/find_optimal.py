e710013bd7525357c77401cd5df02572e5f9c8d0
mystic-cg <215104920@qq.com>
2019-08-28 21:02:18 +0800

    [learning] optimizing the quick sort

--- linear_threshold/find_optimal.py
+++ linear_threshold/find_optimal.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import networkx
 from numpy import random
 
 from linear_threshold.LT_model import linear_threshold
@@ -58,7 +57,58 @@ def quick_sort4tuple_list(unsorted_list, idx=0, is_ordered_by_ascend=True):
     return quick_sort4tuple_list(greater, idx, False) + [mid_tuple] + quick_sort4tuple_list(less, idx, False)
 
 
+def quick_sort_by_recursion(arr):
+    data_len = len(arr)
+    if data_len <= 1:
+        return arr
+    rand_val = random.randint(data_len)
+    mid_val = arr[rand_val]
+    del arr[rand_val]
+    less = [i for i in arr if i <= mid_val]
+    greater = [i for i in arr if i > mid_val]
+    return quick_sort_by_recursion(less) + [mid_val] + quick_sort_by_recursion(greater)
+
+
+def quicksort(arr, left, right):
+    if left < right:
+        mid = partition(arr, left, right)
+        quicksort(arr, left, mid - 1)
+        quicksort(arr, mid + 1, right)
+
+
+def partition(arr, left, right):
+    x = arr[right]
+    i = left - 1  # 初始i指向一个空，保证0到i都小于等于 x
+    for j in range(left, right):  # j用来寻找比x小的，找到就和i+1交换，保证i之前的都小于等于x
+        if arr[j] <= x:
+            i = i + 1
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[i + 1], arr[right] = arr[right], arr[i + 1]  # 0到i 都小于等于x ,所以x的最终位置就是i+1
+    return i + 1
+
+
+def random_quicksort(arr, left, right):
+    if left < right:
+        mid = random_partition(arr, left, right)
+        random_quicksort(arr, left, mid - 1)
+        random_quicksort(arr, mid + 1, right)
+
+
+def random_partition(arr, left, right):
+    mid = random.randint(left, right + 1)  # 生成[left,right]之间的一个随机数
+    arr[mid], arr[right] = arr[right], arr[mid]
+    x = arr[right]
+    i = left - 1  # 初始i指向一个空，保证0到i都小于等于 x
+    for j in range(left, right):  # j用来寻找比x小的，找到就和i+1交换，保证i之前的都小于等于x
+        if arr[j] <= x:
+            i = i + 1
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[i + 1], arr[right] = arr[right], arr[i + 1]  # 0到i 都小于等于x ,所以x的最终位置就是i+1
+    return i + 1
+
+
 if __name__ == '__main__':
-    g = networkx.Graph()
-    g.add_edges_from([(1, 2), (1, 3), (2, 3), (3, 4), (3, 5), (4, 5), (4, 6), (5, 6)])
-    print(find_optimal(g))
+    # g = networkx.Graph()
+    # g.add_edges_from([(1, 2), (1, 3), (2, 3), (3, 4), (3, 5), (4, 5), (4, 6), (5, 6)])
+    # print(find_optimal(g))
+    print(quick_sort_by_recursion([2, 35, 1, 6, 23, 6762, 2435, 90]))
